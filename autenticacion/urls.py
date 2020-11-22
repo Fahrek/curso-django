b7fe444b6e78b74ab06821fafcda028bf252e353
Andres G. Quina <andresgqjob@gmail.com>
2020-11-22 16:19:10 +0100

    login, logout & models

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import VistaRegistro
+from .views import *
 
 urlpatterns = [
     path('registro/', VistaRegistro.as_view(), name="registro"),
+    path('acceder/', acceder, name="acceder"),
+    path('salir/', salir, name="salir"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def acceder(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_usuario, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request, f"Bienvenid@ de nuevo {nombre_usuario}")
+                return redirect("blog")
+            else:
+                messages.success(request, "Los datos son incorrectos")
+        else:
+            messages.success(request, "Los datos son incorrectos")
+
+    form = AuthenticationForm()
+    return render(request, "acceder.html", {"form": form})
+
 
 
 # Create your views here.
@@ -25,3 +46,9 @@ class VistaRegistro(View):
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
             return render(request, "registro.html", {"form": form})
+
+
+def salir(request):
+    logout(request)
+    messages.success(request, f"Tu sesión se ha cerrado correctamente")
+    return redirect("acceder")
